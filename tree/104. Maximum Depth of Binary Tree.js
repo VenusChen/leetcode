108b867443bfd806cf1b3659c76c4b51900472a8
chenxiaoyu <549134941@qq.com>
2020-02-17 18:40:20 +0800

    no message

--- tree/104. Maximum Depth of Binary Tree.js
+++ tree/104. Maximum Depth of Binary Tree.js
@@ -20,3 +20,22 @@ var maxDepth = function(root) {
     }
     return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
 };
+
+var maxDepth = function (root) {
+    if(!root) {
+        return 0;
+    }
+    let queue = [root];
+    let depth = 0;
+    while (queue.length > 0) {
+        let len = queue.length;
+        for(let i = 0; i < len; i++) {
+            let node = queue.shift();
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        depth++;
+    }
+
+    return depth;
+};
